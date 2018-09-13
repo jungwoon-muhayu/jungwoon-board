061e9b434abae540f70116e641356c0ae93dfd33
jungwoon <42905763+jungwoon-muhayu@users.noreply.github.com>
2018-09-13 15:33:11 +0900

    Initial commit

---
+++ src/main/java/com/muhayu/jungwoonboard/JungwoonBoardApplication.java
@@ -0,0 +1,12 @@
+package com.muhayu.jungwoonboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungwoonBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JungwoonBoardApplication.class, args);
+    }
+}
---
+++ src/test/java/com/muhayu/jungwoonboard/JungwoonBoardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.muhayu.jungwoonboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JungwoonBoardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
